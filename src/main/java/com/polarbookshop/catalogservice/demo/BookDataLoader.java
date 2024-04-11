4dcfb8085ffcaa0f00491e9f4279da2584dbf4f5
kartikdhiman <59189590+kartikdhiman@users.noreply.github.com>
2024-04-11 20:56:21 +0530

    chore: load more book data

--- src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
+++ src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -22,7 +22,10 @@ public class BookDataLoader {
 	public void loadBookTestData() {
 		bookRepository.deleteAll();
 		var book1 = Book.of("1234567891", "Northern Lights", "Lyra Silverstar", 9.90, "Polarsophia");
-		var book2 = Book.of("1234567892", "Polar Journey", "Iorek Polarson", 12.90, "Polarsophia");
-		bookRepository.saveAll(List.of(book1, book2));
+		var book2 = Book.of("1234567898", "Polar Journey", "Iorek Polarson", 12.90, "Polarsophia");
+		var book3 = Book.of("1234567899", "Northern Lights", "J.K. Rowling", 9.90, "Bloomsbury Publishing");
+		var book4 = Book.of("1234567892", "Polar Sunset", "George R.R. Martin", 12.90, "Bantam Books");
+		var book5 = Book.of("1234567893", "Desert Mirage", "Stephen King", 15.90, "Scribner");
+		bookRepository.saveAll(List.of(book1, book2, book3, book4, book5));
 	}
 }
